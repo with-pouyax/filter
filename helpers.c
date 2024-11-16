b8e633c54a5bc0cdd3e61ca579fb43fc513a261a
with-pouyax <75066687+with-pouyax@users.noreply.github.com>
2024-11-16 15:31:06 +0100

    added some comments

--- helpers.c
+++ helpers.c
@@ -11,6 +11,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Calculate the average of the red, green, and blue values
+            // so like this always our red, green and blue values will be the same
+            // when all the values are the same, the pixel will be in range of gray color
             float average =
                 (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0;
 
@@ -28,34 +30,35 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    // sepia is a color filter that makes the image look like it was taken in the 19th century
     // Iterate over each row
     for (int i = 0; i < height; i++)
     {
         // Iterate over each pixel in the row
         for (int j = 0; j < width; j++)
         {
-            // Store original color values
+            // Store original color values in temporary variables
             BYTE originalRed = image[i][j].rgbtRed;
             BYTE originalGreen = image[i][j].rgbtGreen;
             BYTE originalBlue = image[i][j].rgbtBlue;
 
             // Calculate sepia values using the sepia formula
             int sepiaRed =
-                round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
+                round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue); // by using the sepia formula we can get the sepia values
             int sepiaGreen =
                 round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
             int sepiaBlue =
                 round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
 
             // Ensure the sepia values do not exceed 255
-            image[i][j].rgbtRed = sepiaRed > 255 ? 255 : sepiaRed;
-            image[i][j].rgbtGreen = sepiaGreen > 255 ? 255 : sepiaGreen;
-            image[i][j].rgbtBlue = sepiaBlue > 255 ? 255 : sepiaBlue;
+            image[i][j].rgbtRed = sepiaRed > 255 ? 255 : sepiaRed; // if the value is greater than 255, we set it to 255
+            image[i][j].rgbtGreen = sepiaGreen > 255 ? 255 : sepiaGreen; // if the value is greater than 255, we set it to 255
+            image[i][j].rgbtBlue = sepiaBlue > 255 ? 255 : sepiaBlue; // if the value is greater than 255, we set it to 255
         }
     }
 }
 
-// Reflect image horizontally
+// Reflect image horizontally (mirror image) by swapping pixels
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     // Iterate over each row
@@ -65,9 +68,10 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width / 2; j++)
         {
             // Swap the pixel with its horizontally mirrored counterpart
-            RGBTRIPLE temp = image[i][j];
+            RGBTRIPLE temp = image[i][j]; // we store the current pixel in a temporary variable
             image[i][j] = image[i][width - 1 - j];
-            image[i][width - 1 - j] = temp;
+            image[i][width - 1 - j] = temp; // we swap the current pixel with the mirrored pixel
+            // mirrored pixel is the pixel that is at the same row but at the end of the row
         }
     }
 }
